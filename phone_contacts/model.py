1e7df45ede07bb86c82ef7e3954f193bb07761a6
eugene <eugene.cherepanov.84>
2023-01-11 00:17:42 +0500

    save

--- phone_contacts/model.py
+++ phone_contacts/model.py
@@ -3,6 +3,7 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtSql import QSqlTableModel
 
+
 class ContactsModel:
     def __init__(self):
         self.model = self._createModel()
@@ -16,5 +17,15 @@ class ContactsModel:
         tableModel.select()
         headers = ("ID", "Name", "Phone", "Email")
         for columnIndex, header in enumerate(headers):
-            tableModel.setHeaderData(columnIndex, Qt.Orientation.Horizontal, header)
+            tableModel.setHeaderData(
+                columnIndex, Qt.Orientation.Horizontal, header)
         return tableModel
+
+    def addContact(self, data):
+        """Add a contact to the database."""
+        rows = self.model.rowCount()
+        self.model.insertRows(rows, 1)
+        for column, field in enumerate(data):
+            self.model.setData(self.model.index(rows, column + 1), field)
+        self.model.submitAll()
+        self.model.select()
--- phone_contacts/views.py
+++ phone_contacts/views.py
@@ -3,10 +3,16 @@
 
 
 from .model import ContactsModel
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QAbstractItemView,
+    QDialog,
+    QDialogButtonBox,
+    QFormLayout,
     QHBoxLayout,
+    QLineEdit,
     QMainWindow,
+    QMessageBox,
     QPushButton,
     QTableView,
     QVBoxLayout,
@@ -39,6 +45,7 @@ class Window(QMainWindow):
         self.table.resizeColumnsToContents()
         # Create buttons
         self.addButton = QPushButton("Add...")
+        self.addButton.clicked.connect(self.openAddDialog)
         self.deleteButton = QPushButton("Delete")
         self.clearAllButton = QPushButton("Clear All")
         # Lay out the GUI
@@ -49,3 +56,68 @@ class Window(QMainWindow):
         layout.addWidget(self.clearAllButton)
         self.layout.addWidget(self.table)
         self.layout.addLayout(layout)
+
+    def openAddDialog(self):
+        """Open the Add Contact dialog."""
+        dialog = AddDialog(self)
+        if dialog.exec() == QDialog.accepted:
+            self.contactsModel.addContact(dialog.data)
+            self.table.resizeColumnsToContents()
+
+
+class AddDialog(QDialog):
+    """Add Contact dialog."""
+    def __init__(self, parent=None):
+        """Initializer."""
+        super().__init__(parent=parent)
+        self.setWindowTitle("Add Contact")
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.data = None
+
+        self.setupUI()
+
+    def setupUI(self):
+        """Setup the Add Contact dialog's GUI."""
+        # Create line edits for data fields
+        self.nameField = QLineEdit()
+        self.nameField.setObjectName("Name")
+        self.phoneField = QLineEdit()
+        self.phoneField.setObjectName("Phone")
+        self.emailField = QLineEdit()
+        self.emailField.setObjectName("Email")
+        # Lay out the data fields
+        layout = QFormLayout()
+        layout.addRow("Name:", self.nameField)
+        layout.addRow("Phone:", self.phoneField)
+        layout.addRow("Email:", self.emailField)
+        self.layout.addLayout(layout)
+        # Add standard buttons to the dialog and connect them
+        self.buttonsBox = QDialogButtonBox(self)
+        self.buttonsBox.setOrientation(Qt.Orientation.Horizontal)
+        self.buttonsBox.setStandardButtons(
+            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+        )
+        self.buttonsBox.accepted.connect(self.accept)
+        self.buttonsBox.rejected.connect(self.reject)
+        self.layout.addWidget(self.buttonsBox)
+    
+    def accept(self):
+        """Accept the data provided through the dialog."""
+        self.data = []
+        for field in (self.nameField, self.phoneField, self.emailField):
+#            if not field.text():
+#                QMessageBox.critical(
+#                    self,
+#                    "Error!",
+#                    f"You must provide a contact's {field.objectName()}",
+#                )
+#                self.data = None  # Reset .data
+#                return
+
+            self.data.append(field.text())
+            
+#        if not self.data:
+#            return
+
+        super().accept()
